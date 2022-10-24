14aa71d3bbddd1811283692b1079eab7237b3afc
mattjmeier <matthew.james.meier@gmail.com>
2022-10-23 22:19:05 -0400

    Clean up CpG analysis, add mm10

--- R/CpG_sites_analysis.R
+++ R/CpG_sites_analysis.R
@@ -1,29 +1,35 @@
 library(tidyverse)
 library(GenomicRanges)
 
-# Step 1 - Import the regions of interest
-get_regions <- function(species = "human") {
+
+get_regions <- function(species) {
+  # Step 1 - Import the regions of interest
   # Load species database
   if (species == "human") {
     db <- "BSgenome.Hsapiens.UCSC.hg38"
     # library(org.Hs.eg.db) # May be useful...
     regions_file = "./inst/genic_regions_hg38.txt"
   } else if (species == "mouse") {
-    library(org.Mm.eg.db)
+    db <- "BSgenome.Mmusculus.UCSC.mm10"
+    #library(org.Mm.eg.db)
     regions_file = "./inst/genic_regions_mm10.txt"
   }
   
   regions <- read.delim(regions_file)
-  regions_ranges <- makeGRangesFromDataFrame(regions,
+  regions_ranges <- makeGRangesFromDataFrame(df = regions,
                                             keep.extra.columns = T,
                                             seqnames.field = "contig",
                                             start.field = "start",
-                                            end.field = "end")
+                                            end.field = "end",
+                                            starts.in.df.are.0based = TRUE)
   
-  seqs <- getSeq(get(db), names = regions_ranges )
-  return(regions)
+  # Step 2 - Get FASTA reference sequences for regions of interest
+  seqs <- getSeq(get(db), names = regions_ranges)
+  mcols(regions_ranges)$sequence <- seqs
+  return(regions_ranges)
 }
 
-# Step 2 - Get FASTA reference sequences for regions of interest
 
 # Step 3 - enumerate all the CpG sites within those regions identified
+
+Biostrings::matchPattern(pattern = "CG", subject = test[[i]])
