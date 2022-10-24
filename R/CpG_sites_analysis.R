a06b8b141649d3d4dc388e4da9c6522884381561
mattjmeier <matthew.james.meier@gmail.com>
2022-10-23 21:53:38 -0400

    Start GC analysis

--- R/CpG_sites_analysis.R
+++ R/CpG_sites_analysis.R
@@ -1,5 +1,29 @@
 library(tidyverse)
+library(GenomicRanges)
 
-# Step 1 - get FASTA reference sequences for regions of interest
+# Step 1 - Import the regions of interest
+get_regions <- function(species = "human") {
+  # Load species database
+  if (species == "human") {
+    db <- "BSgenome.Hsapiens.UCSC.hg38"
+    # library(org.Hs.eg.db) # May be useful...
+    regions_file = "./inst/genic_regions_hg38.txt"
+  } else if (species == "mouse") {
+    library(org.Mm.eg.db)
+    regions_file = "./inst/genic_regions_mm10.txt"
+  }
+  
+  regions <- read.delim(regions_file)
+  regions_ranges <- makeGRangesFromDataFrame(regions,
+                                            keep.extra.columns = T,
+                                            seqnames.field = "contig",
+                                            start.field = "start",
+                                            end.field = "end")
+  
+  seqs <- getSeq(get(db), names = regions_ranges )
+  return(regions)
+}
 
-# Step 2 - enumerate all the CpG sites within those regions identified
+# Step 2 - Get FASTA reference sequences for regions of interest
+
+# Step 3 - enumerate all the CpG sites within those regions identified
