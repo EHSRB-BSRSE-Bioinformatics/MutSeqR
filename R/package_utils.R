70b60cac79e0ee99e86fe92e9dde347ea6f44d14
Annette Dodge <annettedodge16@gmail.com>
2023-09-08 14:41:30 -0400

    Automatic isntalltion of bioconductor pacakges was not working so I removed it and amnow trying a new idea - added biocViews to description file, will test installation

--- R/package_utils.R
+++ R/package_utils.R
@@ -2,32 +2,42 @@
   reticulate::configure_environment(pkgname, force = TRUE)
 }
 
-#' @import BiocManager
-# To do: check if they have them installed before it asks if they want to install pkgs
+# Set an environment variable to control package installation
+Sys.setenv(DUPSEQR_INSTALL_PACKAGES = "0")
+
 .onLoad <- function(libname, pkgname) {
-  # List of Bioconductor packages the package depends on
-  bioconductor_packages <- c("Biostrings", "VariantAnnotation", "trackViewer", "GenomicRanges")
+  install_required_packages <- Sys.getenv("DUPSEQR_INSTALL_PACKAGES")
   
-  missing_packages <- bioconductor_packages[!sapply(bioconductor_packages, requireNamespace, quietly = TRUE)]
-  
-  if (length(missing_packages) > 0) {
-    auto_install_options <- c("Yes, install Bioconductor packages", "No, do not install")
+  if (install_required_packages == "1") {
+    # List of Bioconductor packages the package depends on
+    bioconductor_packages <- c("Biostrings", "GenomeInfoDb", "GenomicRanges", "IRanges", "plyranges", "S4Vectors", "VariantAnnotation")
     
-    user_choice <- menu("Would you like to automatically install required Bioconductor packages?",
-                        choices = auto_install_options, title = "Package Installation")
+    missing_packages <- bioconductor_packages[!sapply(bioconductor_packages, requireNamespace, quietly = TRUE)]
     
-    if (user_choice == 1) {
-      if (!requireNamespace("BiocManager", quietly = TRUE)) {
-        install.packages("BiocManager")
-      }
+    if (length(missing_packages) > 0) {
+      auto_install_options <- c("Yes, install Bioconductor packages", "No, do not install")
       
-      packageStartupMessage("Installing missing Bioconductor packages...")
-      BiocManager::install(missing_packages, dependencies = TRUE)
-      message("Bioconductor packages installed.")
+      user_choice <- menu("Would you like to automatically install required Bioconductor packages?",
+                          choices = auto_install_options, title = "Package Installation")
+      
+      if (user_choice == 1) {
+        if (!requireNamespace("BiocManager", quietly = TRUE)) {
+          install.packages("BiocManager")
+        }
+        
+        packageStartupMessage("Installing missing Bioconductor packages...")
+        BiocManager::install(missing_packages, dependencies = TRUE)
+        message("Bioconductor packages installed.")
+      } else {
+        warning("Some required Bioconductor packages are missing. Please install them manually.")
+      }
     } else {
-      warning("Some required Bioconductor packages are missing. Please install them manually.")
+      packageStartupMessage("All required Bioconductor packages are already installed.")
     }
-  } else {
-    packageStartupMessage("All required Bioconductor packages are already installed.")
   }
 }
+
+# In your package installation code, set the environment variable to "1" to trigger package installation
+# Sys.setenv(DUPSEQR_INSTALL_PACKAGES = "1")
+# install.packages("YourPackage")
+
