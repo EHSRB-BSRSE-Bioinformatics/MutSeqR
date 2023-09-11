a989f0b0aaf404c139b14e06a3d952c850659c05
Annette Dodge <annettedodge16@gmail.com>
2023-09-11 15:40:38 -0400

    variables within dplyr functions need to be defined as rlang::.data$variable. Changed in all instances. I also ensured that all functions are defined by their package

--- R/signature_analysis.R
+++ R/signature_analysis.R
@@ -12,12 +12,13 @@
 #' Run COSMIC signatures comparison
 #'
 #' After cleaning the mutation data input, runs several Alexandrov Lab tools for COSMIC signature analysis (assigns signatures to best explain the input data).
-#' @param mutation_data A data frame imported from a .mut file
+#' @param mutations A data frame, "mutation_data", imported from a .mut file
 #' @param project_name The name of the project; used to get mutation data into the required .txt format for SigProfiler
 #' @param project_genome A string describing the reference genome to use; e.g., GRCh38
-#' @param output_path The directory where output results should be written
+#' output_path The directory where output results should be written. *not a parameter of the function
 #' @param group The column in the mutation data used to aggregate groups (e.g., sample ID, tissue, dose)
-#' @param python_path The path to the version of python to be used with reticulate. It is important that this version of python meets the dependecies, including the SigProfiler python tools.
+#' @param python_path The path to the version of python to be used with reticulate. It is important that this version of python meets the dependencies, including the SigProfiler python tools.
+#' @param python_home The path to the conda virtual environment that contains the required python dependencies
 #' @returns Creates a subfolder in the output directory with SigProfiler tools results.
 #'  SigProfilerAssignmentR cosmic_fit
 #'  SigProfilerExtractorR sigprofilerextractor
@@ -25,6 +26,7 @@
 #' @importFrom here here
 #' @importFrom dplyr filter select rename mutate relocate 
 #' @importFrom utils write.table
+#' @importFrom rlang .data
 #' @import reticulate
 #' @import stringr
 #' @export
@@ -79,8 +81,8 @@ signature_decomposition <- function(mutations = mutation_data,
 
   # Clean data into required format for Alexandrov Lab tools...
   signature_data <- as.data.frame(mutations) %>%
-    dplyr::filter(!variation_type %in% "no_variant") %>%
-    dplyr::select(all_of(group), id, variation_type, seqnames, start, end, ref, alt) %>%
+    dplyr::filter(!.data$variation_type %in% "no_variant") %>%
+    dplyr::select(all_of(group), .data$id, .data$variation_type, seqnames, .data$start, .data$end, .data$ref, .data$alt) %>%
     dplyr::rename(
       "Sample" = group,
       "ID" = "id",
@@ -89,13 +91,13 @@ signature_decomposition <- function(mutations = mutation_data,
       "pos_start" = "start",
       "pos_end" = "end"
     ) %>%
-    dplyr::mutate(Sample = stringr::str_replace_all(Sample, " ", "_")) %>%
-    dplyr::mutate(chrom = stringr::str_replace(chrom, "chr", "")) %>%
+    dplyr::mutate(Sample = stringr::str_replace_all(.data$Sample, " ", "_")) %>%
+    dplyr::mutate(chrom = stringr::str_replace(.data$chrom, "chr", "")) %>%
     dplyr::mutate(Project = project_name) %>%
     dplyr::mutate(Genome = project_genome) %>%
     dplyr::mutate(Type = "SOMATIC") %>%
-    dplyr::relocate(Project) %>%
-    dplyr::relocate(Genome, .after = ID) %>%
+    dplyr::relocate(.data$Project) %>%
+    dplyr::relocate(.data$Genome, .after = .data$ID) %>%
     dplyr::mutate(mut_type = "SNP") # This should be fixed before using on other datasets.
 
   output_path <- file.path(
