32a3533f7efdb189fc495d23ab8c790f2f8f8a65
Annette Dodge <annettedodge16@gmail.com>
2024-01-16 17:06:47 -0500

    Modified the signature-fitting function to work. I replaced the signatures.py with a simpler code. Fixed output path. Asks users if they want to proceed with installation of dependencies before doing so.

--- R/signature_fitting.R
+++ R/signature_fitting.R
@@ -7,7 +7,8 @@
 #' After cleaning the mutation data input, runs several Alexandrov Lab tools for COSMIC signature analysis (assigns signatures to best explain the input data).
 #' @param mutations A data frame, imported from a .mut file
 #' @param project_name The name of the project; used to get mutation data into the required .txt format for SigProfiler
-#' @param project_genome A string describing the reference genome to use; e.g., GRCh38
+#' @param project_genome A string describing the reference genome to use; 
+#' e.g. GRCh37, GRCH38, mm10, mm9, rn6
 #' output_path The directory where output results should be written. *not a parameter of the function
 #' @param group The column in the mutation data used to aggregate groups (e.g., sample ID, tissue, dose)
 #' @param python_path The path to the version of python to be used with reticulate. It is important that this version of python meets the dependencies, including the SigProfiler python tools.
@@ -26,38 +27,55 @@
 signature_fitting <- function(mutations,
                               project_name = "Default",
                               project_genome = "GRCh38",
+                              env_name = "DupSeqR",
                               group = "sample",
                               output_path = NULL,
-                              python_version = "3.11:latest", # We should test with other versions
+                              python_version = "3.11", # We should test with other versions
                               python_path = "~/../../AppData/Local/Programs/Python/Python310/python.exe", #"/usr/bin/python3.9"
-                              python_home = "~/.virtualenvs/r-reticulate") {
+                              python_home = "C:/Users/adodge/OneDrive - HC-SC PHAC-ASPC/Documents/.virtualenvs/r-reticulate") {
   if (!requireNamespace("reticulate")) {
     stop("Reticulate not installed: you need this to run SigProfiler tools in R.")
   }
-  message("This function will attempt to install python using reticulate, as
-          this is a requirement for the SigProfiler suite of tools.
+  message("Note: This function requires python to be installed on the users 
+          computer. If you do not have python installed, you can do so using: 
+          reticulate::install_python().
+          \n\nThis function will create a virtual environment using reticulate to 
+          run python, as this is a requirement for the SigProfiler suite of tools.
           Note that it will also install several python dependencies using
           a conda virtual environment on first use. Please be aware of the 
           implications of this. For advanced use, it is suggested to
           use the SigProfiler python tools directly in python as described
           in their respective documentation.")
+ 
+  # Only run this once, not every time that the function is called
+  #python_version <- "3.11:latest
   
-  # Only run this once, not ever time function is called
-  #python_version <- "3.11:latest"
   installed_envs <- reticulate::virtualenv_list()
   # Check if DupSeqR virtualenv already exists
-  if ("DupSeqR" %in% installed_envs) {
+  if (env_name %in% installed_envs) {
     # Virtualenv exists, use it
-    reticulate::use_virtualenv("DupSeqR")
+    reticulate::use_virtualenv(env_name)
   } else {
-    # Virtualenv doesn't exist, set it up
-    reticulate::virtualenv_create("DupSeqR", python = python_version)
-    # Install required packages
-    reticulate::virtualenv_install("DupSeqR", c("SigProfilerMatrixGenerator", "SigProfilerAssignment", "SigProfilerExtractor"))
-  }
+    # Ask the user for confirmation
+    user_input <- utils::menu("Do you want to create a virtual environment and 
+                              install the required Python packages? This may 
+                              take several minutes", 
+                              title = "Confirmation", choices = c("Yes", "No"))
+    
+    if (user_input == 1) {
+      # User chose to install the packages
+      # Virtualenv doesn't exist, set it up
+      reticulate::virtualenv_create(env_name, python = reticulate::virtualenv_starter(python_version))
+      # Install required packages
+      reticulate::virtualenv_install(env_name, c("SigProfilerMatrixGenerator", "SigProfilerAssignment", "SigProfilerExtractor"))
+    } else {
+      # User chose not to install the packages
+      cat("Installation aborted by the user.\n")
+      stop("Function terminated.")
+    } }
   
   # reticulate::install_python(version = python_version)
-  reticulate::use_virtualenv("DupSeqR")
+  reticulate::use_virtualenv(env_name)
   
   # This will only install the genome if it's not found in the current env
   SigProfilerMatrixGeneratorR::install(project_genome)
@@ -69,21 +87,35 @@ signature_fitting <- function(mutations,
   # Clean data into required format for Alexandrov Lab tools...
   #ID doesn't always exist. 
   # New F(x) to create MAF/txt input for web sigprofiler
-  signature_data <- as.data.frame(mutations) %>%
+  signature_data <- as.data.frame(mutations) 
+  
+  # Check if "id" column exists
+  if (!"id" %in% colnames(signature_data)) {
+    # If not, create "id" column and populate with "."
+   signature_data$id <- "."
+  }
+  
+  # Check if "seqnames" column exists (ie if it came from a GRanges)
+  if ("seqnames" %in% colnames(signature_data)) {
+    # If "seqnames" exists, rename it to "contig"
+    signature_data <- dplyr::rename(signature_data, contig = seqnames)
+  }
+  
+  signature_data <- signature_data %>%
     dplyr::filter(.data$variation_type %in% "snv") %>%
-    #dplyr::filter(.data$is_germline == FALSE) %>%   
-    dplyr::select(all_of(group), .data$id, .data$variation_type, seqnames, .data$start, .data$end, .data$ref, .data$alt) %>%
+    dplyr::filter(.data$is_germline == FALSE) %>%   
+    dplyr::select(all_of(group), .data$id, .data$variation_type, contig, .data$start, .data$end, .data$ref, .data$alt) %>%
     dplyr::rename(
       "Sample" = group,
       "ID" = "id",
       "mut_type" = "variation_type",
-      "chrom" = "seqnames",
+      "chrom" = "contig",
       "pos_start" = "start",
       "pos_end" = "end"
     ) %>%
     dplyr::mutate(Sample = stringr::str_replace_all(.data$Sample, " ", "_")) %>%
     dplyr::mutate(chrom = stringr::str_replace(.data$chrom, "chr", "")) %>%
-    dplyr::mutate(Project = project) %>%
+    dplyr::mutate(Project = project_name) %>%
     dplyr::mutate(Genome = project_genome) %>%
     dplyr::mutate(Type = "SOMATIC") %>%
     dplyr::relocate(.data$Project) %>%
@@ -91,7 +123,7 @@ signature_fitting <- function(mutations,
     dplyr::mutate(mut_type = "SNP") # This should be fixed before using on other datasets.
   
   message("Generating output path string...")
-  if (!is.null(output_path)) {
+  if (is.null(output_path)) {
     output_path <- file.path(
       here::here(),
       "output",
--- inst/extdata/signatures.py
+++ inst/extdata/signatures.py
@@ -1,12 +1,6 @@
 import sys
-import SigProfilerAssignment as spa
 from SigProfilerAssignment import Analyzer as Analyze
-from SigProfilerExtractor import sigpro as sig
-from SigProfilerMatrixGenerator import install as genInstall
-
-def install_genome(genome, custom, bash):
-  genInstall.install(genome, rsync=False, bash=False, ftp=True, custom = False)
-
+ 
 def cosmic_fit_DupSeqR(samples,
                        output,
                        genome_build="GRCh38",
@@ -21,14 +15,4 @@ def cosmic_fit_DupSeqR(samples,
                        cosmic_version=3.3,
                        verbose=True,
                        exome=False)
-
-def sigProfilerExtractorDupSeqR(sample_file, output_dir, reference_genome):
-  sig.sigProfilerExtractor(input_data = sample_file,
-                       output = output_dir,
-                       input_type = "matrix",
-                       opportunity_genome = reference_genome,
-                       cosmic_version=3.3,
-                       minimum_signatures=1,
-                       maximum_signatures=10,
-                       nmf_replicates=100)
 
