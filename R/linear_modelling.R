1651d5eb4a6f2fa04b0aa7f658d3e297fb95686f
Annette Dodge <annettedodge16@gmail.com>
2024-01-30 16:52:50 -0500

    GLMM function can now do pairwise comparisons. Made my.holm.sidak into its own function. Ready for testing

--- R/linear_modelling.R
+++ R/linear_modelling.R
@@ -206,28 +206,8 @@ glm_mf_by_factor <- function(mf_data,
   new_row_names <- paste(contrast_table[, 1], "vs.", contrast_table[ , 2])
   # Rename the rows in the estimates table
   rownames(pairwise_comparisons) <- new_row_names
-  
-  ###############################################
-  # Holm-Sidak Correction for multiple comparison
-  my.holm.sidak <- function(P){
-    m <- length(P)
-    if(m > 1){
-      Psort <- matrix(c(P, 1:m), 2, m, byrow = TRUE)
-      Psort <- Psort[, order(Psort[1, ])]
-      for (i in 1:m) {
-        adjust <- m + 1 - i
-        Psort[1, i] <- pmin(1, (1 - ((1 - Psort[1, i])^adjust)))
-      }
-      Psort <- Psort[, order(Psort[2, ])]
-      P.adjust <- Psort[1, ]
-      P.adjust
-    } else{
-      #No adjustment as there is only one comparison
-      P.adjust <- P
-      P.adjust
-    }}
-    
-    my.holm.sidak(pairwise_comparisons$p.value)
+  # Correct for multiple comparisons
+  pairwise_comparisons$adj_p.value <- DupSeqR::my.holm.sidak(pairwise_comparisons$p.value)
 
  ############################################### 
   # Return output file
@@ -254,11 +234,12 @@ glm_mf_by_factor <- function(mf_data,
 #' Mutation counts and total_depth should be summarized per sample alongside a
 #' columns for your fixed effects. 
 #' This data can be obtained using the calculate_mut_freq(summary = TRUE). 
-#' @param fixed_effects This should be the name of the columns that will act as the 
+#' @param fixed_effects This should be the name(s) of the column(s) that will act as the 
 #' factor/fixed effect for modelling mutation frequency. Fixed effects encapsulate 
 #' the tendencies/trends that are consistent at the levels of primary interest. 
 #' These effects are considered fixed because they are non-random and assumed to 
-#' be constant for the samples being studied. Ex. 'fixed_effects = c("dose", "target")'.
+#' be constant for the samples being studied. 
+#' Ex. 'fixed_effects = c("dose", "target", "tissue", "age", etc)'.
 #' @param test_interaction TRUE or FALSE. Whether or not your model should test 
 #' the interaction between fixed_effects on top of the effect of each factor.
 #' @param random_effects This should be the name of the column to be analysed as 
@@ -266,45 +247,43 @@ glm_mf_by_factor <- function(mf_data,
 #' at different levels of the data hierarchy. These account for the unmeasured 
 #' sources of variance that affect certain groups in the data. Ex. If your model
 #' uses repeated measures of sample, 'random_effects = "sample"'. 
-#' @param reference_level Refers to one of the levels within your fixed_effects. 
+#' @param reference_level Refers to one of the levels within each of your fixed_effects. 
 #' Remaining levels will be contrasted against the reference_level.
 #' Ex. if my factor was a dose column, then my reference_level would refer to my
 #' negative control dose. 
 #' @param muts The column containing the mutation count per sample.
 #' @param total_count The column containing the sequencing depth per sample.
 #' @param contrast_table_file a filepath to a tab-delimited .txt file that will 
-#' provide the information necessary to make comparisons between samples. 
-#' The first column must be a level within your factor and the second column 
-#' must be the level within your factor that it will be compared to. The names 
-#' must correspond to entries in your mf_data factor column. Put the factor that
-#' you expect to have the higher mutation frequency in the 1st column and the 
-#' factor level that you expect to have a lower mutation frequency in the second 
-#' column. For example, if your factor is "dose" with dose groups 0, 25, 50, 100, 
-#' then the first column would contain the treated groups (25, 50, 100), while 
-#' the second column would be 0, thus comparing each treated group to the 
-#' control group. The contrast table for this example would be: 
+#' provide the information necessary to make comparisons between groups. 
+#' The tale must contain two columns. The first column will be a group within 
+#' your fixed_effects and the second column must be the group that it will be 
+#' compared to. For multiple fixed-effects, separate the levels of each fixed-effect
+#' with a colon. Ex. For fixed effects "dose" and "target", 0:chr1 would
+#' specify dose 0, target chr1. The values must correspond to entries in your 
+#' mf_data factor column. Put the group that you expect to have the higher 
+#' mutation frequency in the 1st column and the group that you expect to have a 
+#' lower mutation frequency in the second column. For example, if you have a fixed 
+#' effect "dose" with dose groups 0, 25, 50, 100, then the first column would 
+#' contain the treated groups (25, 50, 100), while the second column would be 0, 
+#' thus comparing each treated group to the control group. 
+#' For example, if the fixed effects are "dose" (0, 25, 50, 100) and "target"
+#' ("chr1", "chr2"), and you would like to compare the three treated
+#' dose groups to the control for each target, then the contrast table would look like:
 #' 
-#' 25 0 
+#' 25:chr1	0:chr1
 #' 
-#' 50 0 
+#' 50:chr1	0:chr1
 #' 
-#' 100 0 
+#' 100:chr1	0:chr1
 #' 
-#' Alternatively, if you would like to compare mutation frequency between treated
-#' dose groups, than the contrast table would look as follows, with the lower
-#' dose always in the second column, as we expect it to have a lower mutation 
-#' frequency.
+#' 25:chr2	0:chr2
 #' 
-#' 100 25
+#' 50:chr2	0:chr2
 #' 
-#' 100 50
-#' 
-#' 50 25
-#' 
-#' 
+#' 100:chr2	0:chr2
 #' 
 #' 
-#' @returns A data frame with the model estimates and specified pairwise comparisons
+#' @returns A list of results including the model estimates and specified pairwise comparisons
 #' @importFrom magrittr %>%
 #' @importFrom doBy esticon
 #' @importFrom lme4 glmer
@@ -317,7 +296,7 @@ glmm_mf <- function(mf_data,
                     fixed_effects = c("dose", "label"),
                     test_interaction = TRUE,
                     random_effects = "sample",
-                    reference_level = 0,
+                    reference_level = c(0, "chr1"),
                     muts = "sample_label_sum_unique",
                     total_count = "sample_label_group_depth",
                     contrast_table_file =  NULL) {
@@ -325,30 +304,25 @@ glmm_mf <- function(mf_data,
   # Convert specified columns to factors
   mf_data[, fixed_effects] <- lapply(mf_data[, fixed_effects], as.factor) 
   
-  # Extract unique levels from each factor and combine them
-  factor_levels <- unique(unlist(lapply(fixed_effects, function(factor_name) {
-    factor_variable <- mf_data[[factor_name]]
-    levels(factor_variable)
-  })))
-  
-  # Check that the reference level is a level of the factor
-  reference_valid <- reference_level %in% factor_levels
-  if (!reference_valid) {
-    stop(paste(reference_level, "is not a valid reference_level. Please ensure that your 
-                  reference_level corresponds to a level of your factor column"))
+    # Check that the reference levels are valid levels of the factors
+  reference_valid <- all(sapply(seq_along(fixed_effects), function(i) {
+    factor_name <- fixed_effects[i]
+    factor_levels <- levels(mf_data[[factor_name]])
+    invalid_levels <- reference_level[i][!reference_level[i] %in% factor_levels]
+    
+    if (length(invalid_levels) > 0) {
+      stop(paste("Invalid reference level(s) for factor", factor_name, ":", paste(invalid_levels, collapse = ", ")))
+    }
+    return(TRUE)
+  }))
+
+  # Set the reference level for each factor in fixed_effects
+  for (factor_name in fixed_effects) {
+    reference_level_for_factor <- reference_level[match(factor_name, fixed_effects)]
+    mf_data[[factor_name]] <- relevel(mf_data[[factor_name]], ref = reference_level_for_factor)
   }
   
-  # factor_level <- lapply(fixed_effects, function(factor_name) {
-  #  if(reference_level %in% levels(mf_data[[factor_name]])){
-  #    mf_data[[factor_name]] <- stats::relevel(mf_data[[factor_name]], ref = as.character(reference_level))
-  #  }
-  #  levels(mf_data[[factor_name]])
-  # })
-  
-  # Set the reference_level; reorders the factor levels with reference_level first
-  # mf_data[[factor]] <- stats::relevel(mf_data[[factor]], ref = as.character(reference_level))
-  
-  # Construct the formula
+  # Construct the model formula
   if(test_interaction){
     formula_str <- paste("cbind(", muts, ",", total_count, ") ~ ", paste(fixed_effects, collapse = "*"))
   } else {
@@ -366,10 +340,10 @@ glmm_mf <- function(mf_data,
   
   # We should have a think about the control and how it can be made flexible. 
   model <- lme4::glmer(glmm_formula, 
-                 family = "binomial", 
+                 family = "binomial", # Ask Andrew: if we introduce different fixed effects, will we need to change this?
                  data = mf_data, 
                  control = lme4::glmerControl(check.conv.grad = lme4::.makeCC("warning", 
-                                                                  tol = 3e-3, 
+                                                                  tol = 3e-3, ## Ask Andrew if we can parameterise this
                                                                   relTol = NULL)))
   
   model_summary <- summary(model)
@@ -377,35 +351,20 @@ glmm_mf <- function(mf_data,
   model_anova <-car::Anova(model)
  #################################
   # Check residuals
-  mf_data$Resid <- stats::residuals(model)
-  # Print the entire row with the maximum residual
-  cat("The row with the maximum residual is:\n")
-  mf_data[abs(stats::residuals(model)) == max(abs(stats::residuals(model))),]
+  mf_data$residuals <- stats::residuals(model)
+ 
+   # Print the row with the maximum residual
+  max_residual_index <- which.max(abs(mf_data$residuals))
+  max_residual_row <- mf_data[max_residual_index, ]
+  message("The row with the maximum residual is:\n")
+  print(max_residual_row)
   
   ## TO DO: Let's walk users through what the residuals should look like and give advice on them
   
-  # Create a data frame with the variables needed for the boxplot
-  plot_data <- mf_data %>%
-    dplyr::mutate(mutation_frequency = mf_data[[muts]] / mf_data[[total_count]])
-  
-  # Create a boxplot to visualise data
-  # Constructing formula for boxplot
-  plot_formula <- paste("mutation_frequency ~", paste(fixed_effects, collapse = "*"))
-  plot_formula <- stats::as.formula(plot_formula)
-  par(las = 2)
-  boxplot(plot_formula,
-          data = plot_data,
-          main = "Mutation Frequency by Fixed Effects",
-          xlab = paste(fixed_effects[1],"x", fixed_effects[2]),
-          ylab = "Mutation Frequency")
-  
-  
   par(las = 1)
   hist <- hist(stats::residuals(model), main = "Residuals")
-  cat(" \n Printing histogram of model residuals: \n")
-  print(hist)
   
-  stats::qqnorm(stats::residuals(model))
+  qqplot <- stats::qqnorm(stats::residuals(model))
   abline(h = -3, col = "red", lwd = 2)
   abline(h = 3, col = "red", lwd = 2)
   
@@ -434,7 +393,7 @@ glmm_mf <- function(mf_data,
   rownames(model_matrix) <- row_names 
   
   # Computed estimates
-  model_estimates <-esticon(model,lambda)
+  model_estimates <-esticon(obj = model, L = model_matrix)
 
   model_estimates <- as.data.frame(model_estimates)
   model_estimates$estimate <- exp(model_estimates$estimate)
@@ -447,7 +406,71 @@ glmm_mf <- function(mf_data,
   colnames(model_estimates) <- c("Estimate", "Std.Err", "Lower", "Upper")
 
   ##############################################################
+ # Pairwise Comparisons  
+ ##################################################################
+  # load contrast table file and do checks
+  if (!is.null(contrast_table_file)) {
+    contrast_table <- read.delim(file.path(contrast_table_file), sep = "\t",
+                                 header = F)
+  } else {
+    cat("Please supply the file path to your contrast table")
+  }
+  if (ncol(contrast_table) <= 1) {
+    stop("Your contrast_table only has one column. Make sure your file is tab-delimited.")
+  }
+  # all_valid <- all(contrast_table %in% fixed_effects_levels)
+  # if (!all_valid) {
+  #   invalid_values <- contrast_table[!(contrast_table %in% fixed_effects_levels)]
+  #   stop(paste("Invalid values in contrast_table:", paste(invalid_values, collapse = ","), 
+  #              ". Please ensure that values in the contrast_table correspond to values in your factor column."))
+  # }
   
+  model_matrix <- as.data.frame(model_matrix)
+  contrast_table <- as.data.frame(contrast_table)  # Convert to data frame if needed
+  
+  # Create an empty list to store the result of matrix subtractions
+  result_list <- list()
+  
+  # Loop through each row in contrast_table
+  for (i in 1:nrow(contrast_table)) {
+    
+    # Get the model_row values
+    V1 <- contrast_table[i, 1]
+    V2 <- contrast_table[i, 2]
+    
+    # Perform matrix subtraction and store the result in the list
+    result_list[[i]] <- model_matrix[V1, ] - model_matrix[V2, ]
+  }
+  
+  # Convert the list of matrices to a single matrix
+  result_matrix <- as.matrix(do.call(rbind, result_list))
+  # Set row names for result_matrix
+  rownames(result_matrix) <- paste(contrast_table[, 1], "vs", contrast_table[, 2])
+  
+ # Perform comparisons
+  pairwise_comparisons <-esticon(obj = model, L = result_matrix)
+
+  # Clean results  
+  pairwise_comparisons <- as.data.frame(pairwise_comparisons)
+  pairwise_comparisons$estimate <- exp(pairwise_comparisons$estimate)
+  delta <- pairwise_comparisons$estimate^2
+  pairwise_comparisons$lwr <- exp(pairwise_comparisons$lwr)
+  pairwise_comparisons$upr <- exp(pairwise_comparisons$upr)
+  pairwise_comparisons$std.error <- sqrt(delta*pairwise_comparisons$std.error^2)
+  pairwise_comparisons <- pairwise_comparisons[,-5]
+  colnames(pairwise_comparisons) <- c("Estimate", "Std.Err", "Obs.T", "p.value", "df", "Lower", "Upper")
+  
+  pairwise_comparisons$adj_p.value <- DupSeqR::my.holm.sidak(pairwise_comparisons$p.value)
+  
+  glmm_results <- list(model_data = mf_data, 
+                      GLMM_formula = glmm_formula,
+                      Residuals_histogram = hist,
+                      Residuals_QQ_plot = qqplot,
+                      point_estimates_matrix = model_matrix,
+                      model_estimates = model_estimates,
+                      pairwise_comparisons_matrix = result_matrix,
+                      pairwise_comparisons = pairwise_comparisons)
+  return(glmm_results)
   
 }
   
