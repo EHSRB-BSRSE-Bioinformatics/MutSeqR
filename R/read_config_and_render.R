4b1f11e260ab2657a93e24e86ea17ebdeb591c45
mattjmeier <matthew.james.meier@gmail.com>
2024-03-01 21:25:57 -0500

    Add render function

---
+++ R/read_config_and_render.R
@@ -0,0 +1,51 @@
+#' Read configuration file and render R Markdown document
+#'
+#' This function reads a configuration file in YAML format, extracts the parameters,
+#' and renders an R Markdown document using the specified parameters.
+#'
+#' @param config_filepath The path to the configuration file.
+#' @param output_file The path to the output file.
+#'
+#' @return None
+#'
+#' @examples
+#' read_config_and_render("config.yaml", "output.html")
+#'
+#' @import yaml
+#' @import rmarkdown
+#' @importFrom utils install.packages
+read_config_and_render <- function(config_filepath, output_file) {
+  config_filepath <- "./config.yml"
+  output_file <- "./output.html"
+  # Check if yaml package is available, install if not
+  if (!requireNamespace("yaml", quietly = TRUE)) {
+    install.packages("yaml")
+  }
+
+  # Check if rmarkdown package is available, install if not
+  if (!requireNamespace("rmarkdown", quietly = TRUE)) {
+    install.packages("rmarkdown")
+  }
+  
+  # take config_filepath and make sure it is a valid file, convert to path if needed
+  config_filepath <- normalizePath(config_filepath)
+  if (!file.exists(config_filepath)) {
+    stop("Config file not found")
+  }
+  
+  # Read the configuration file
+  config <- yaml::yaml.load_file(config_filepath)
+  
+  # Use provided params or default to list if none provided
+  #params <- ifelse(!is.null(config$params), config$params, list())
+  # Construct the path to the .Rmd file within the installed package directory
+  rmd_file <- "DS_summary_report.Rmd"
+  rmd_path <- system.file("extdata", rmd_file, package = "MutSeqR", mustWork = TRUE)
+  # Rendering the R Markdown document
+  rmarkdown::render(
+    input = rmd_path,
+    output_file = output_file,
+    params = config$params,
+    envir = new.env()
+  )
+}
