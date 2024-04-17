38661326f15e3ec9c5256305de8b513087ac112a
Annette Dodge <annettedodge16@gmail.com>
2024-04-17 09:01:43 -0400

    made a radar plot function

--- R/radar_plot.R
+++ R/radar_plot.R
@@ -1,52 +1,63 @@
 #' Create a radar plot
+#' @importFrom tidyr pivot_wider
+
 
 # radar chart
-radar_plot <- function(data,
-                      group){
+radar_plot <- function(mf_data,
+                      response_col,
+                      label_col,
+                      facet_col) {
 
-plot_data <- MF_per_target %>%
-  group_by(label, treatment) %>%
-  summarise(MF = mean(sample_label_MF_unique), Std.Err = sd(sample_label_MF_unique)/sqrt(n()))
+  if (!requireNamespace("fmsb")) {
+    stop("You need the package fmsb to run this function.")
+  }
+  plot_data <- mf_data %>%
+    dplyr::select({{response_col}}, {{label_col}}, {{facet_col}}) %>%
+    tidyr::pivot_wider(names_from = {{label_col}}, values_from = {{response_col}})
 
-radar_data <- plot_data %>%
-  select(label, treatment, MF) %>%
-  pivot_wider(names_from = label, values_from = MF)
-View(radar_data)
-treatments <- radar_data$treatment  
-View(radar_data)
-plots <- list()
-for (i in seq_along(treatments)) {
- treat = treatments[i]
- df_i <- radar_data %>%
-  dplyr::filter(treatment == treat) %>%
-  select(-treatment)
+    # Convert dose column to a factor to handle arbitrary values
+    plot_data[[facet_col]] <- as.factor(plot_data[[facet_col]])
+    # Get levels
+    facet_levels <- levels(plot_data[[facet_col]])
+  
+  # Set up the layout for the plots
+  n_plots <- length(facet_levels)
+  n_cols <- 2  # Number of columns in the grid
+  n_rows <- ceiling(n_plots / n_cols)  # Number of rows needed
+  layout(matrix(1:n_plots, nrow = n_rows, ncol = n_cols, byrow = TRUE))
 
-count <- ncol(df_i)
-# Add rows for max and min values
-max <- max(df_i, na.rm = TRUE) * 1.1
-max_row <- rep(max, count)
-min_row <- rep(0, count)
-df <- rbind(max_row, min_row, df_i)
-axis_labels <- seq(from = 0, to = max, length.out = 5)
-axis_labels <- sprintf("%.1e", axis_labels)
-title <- paste(treat)
-plot <- radarchart(df = df,
-            axistype = 1, # 1: center axis, 2: circle axis, 3 both center and around
-            caxislabels = axis_labels,
-            sep = 5, # number of axis ticks
-            caxiscol = "grey",
-            vlabels = NULL, # variable labels
-            axislabcol = "grey",
-            vlcex = 0.7,  # variable label font size
-            title = title,
-            pcol = "black", # color of the polygon
-            pfcol = NULL, # color of the polygon fill
-            plwd = 2, # width of the polygon line
-            plty = 1, # type of the polygon line
-            cglcol = "grey",
-            cglty = 1,
-            cglwd = 0.7,
-            )
-plots[[i]] <- plot
-}
+  for (i in seq_along(facet_levels)) {
+    facet <- facet_levels[i]
+    df_i <- plot_data %>%
+      dplyr::filter(.data[[paste0(facet_col)]] == facet) %>%
+      select(-{{facet_col}})
+
+    count <- ncol(df_i)
+    # Add rows for max and min values
+    max <- max(df_i, na.rm = TRUE) * 1.1
+    max_row <- rep(max, count)
+    min_row <- rep(0, count)
+    df <- rbind(max_row, min_row, df_i)
+    axis_labels <- seq(from = 0, to = max, length.out = 5)
+    axis_labels <- sprintf("%.1e", axis_labels)
+    title <- paste(facet_col, facet)
+    plot <- fmsb::radarchart(df = df,
+                axistype = 1,
+                caxislabels = axis_labels,
+                sep = 5, # number of axis ticks
+                caxiscol = "grey",
+                vlabels = NULL, # variable labels
+                axislabcol = "grey",
+                vlcex = 0.7,  # variable label font size
+                title = title,
+                pcol = "black", # color of the polygon
+                pfcol = NULL, # color of the polygon fill
+                plwd = 2, # width of the polygon line
+                plty = 1, # type of the polygon line
+                cglcol = "grey",
+                cglty = 1,
+                cglwd = 0.7)
+    }
+layout(1)
+dev.off()
 }
