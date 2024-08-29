dbda1e978ca3244e2989fb743fac76ff1ab21af7
Annette Dodge <annettedodge16@gmail.com>
2024-08-29 15:24:01 -0400

    proast

--- R/proast_bmd.r
+++ R/proast_bmd.r
@@ -6,55 +6,37 @@
 proast_bmd <- function(mf_data,
                        dose_col = "dose",
                        response_col = "sample_MF_min",
+                       covariate_col = NULL,
                        CES = 0.5,
                        adjust_CES_to_group_SD = FALSE,
-                       model_options = "all",
-                       model_selection = "m3 or m5",
                        model_averaging = TRUE,
                        num_bootstraps = 200,
                        summary = TRUE) {
 
   CES_sd <- as.numeric(adjust_CES_to_group_SD) + 1
-
-  # Model Options
-  if (model_options == "expon") {
-    model.options <- "Exponential model only"
-  } else if (model_options == "expon_hill") {
-    model.options <- "Exponential and Hill model"
-  } else if (model_options == "expon_hill_invexpon") {
-    model.options <- "previous option with inverse exponential model added"
-  } else if (model_options == "all") {
-    model.options <- "previous option with lognormal DR model added"
+  
+  if(is.null(covariate_col)) {
+    covariate <- 0
   } else {
-    stop("Invalid model_options argument. Please choose from 'expon', 'expon_hill', 'expon_hill_invexpon', or 'all'.")
+    covariate <- covariate_col
   }
 
-  # Model Selection
-  if (model_selection == "m3") {
-    model.selection <- "select model 3 from various nested families of models"
-  } else if (model_selection == "m5") {
-    model.selection <- "select model 5 from various nested families of models"
-  } else if (model_selection == "m3_m5") {
-    model.selection <-  "select model 3 or 5 from various families of models"
-  } else {
-     stop("Invalid model_selection argument. Please choose from 'm3', 'm5', or 'm3_m5'.")
-  }
-
-  results <- f.proast(mf_data, interactive_mode = FALSE,
-                     datatype = "continuous, individual data",
-                     model_choice = model.selection,
-                     setting_choice = "set of models",
-                     indep_var_choice = dose_col,
-                     Vyans_input = response_col,
-                     covariates = 0,
-                     CES = CES,
-                     adjust_CES_to_group_SD = CES_sd,
-                     model_selection = model.options,
-                     lower_dd = NULL,
-                     upper_dd = NULL,
-                     selected_model = "exponential",
-                     model_averaging = model_averaging,
-                     num_bootstraps = num_bootstraps)
+  results <- f.proast(mf_data,
+                      interactive_mode = FALSE,
+                      datatype = "continuous, individual data",
+                      model_choice = "select model 3 or 5 from various families of models",
+                      setting_choice = "set of models",
+                      indep_var_choice = dose_col,
+                      Vyans_input = response_col,
+                      covariates = covariate,
+                      custom_CES = CES,
+                      adjust_CES_to_group_SD = CES_sd,
+                      model_selection = "previous option with lognormal DR model added",
+                      lower_dd = NULL,
+                      upper_dd = NULL,
+                      selected_model = "exponential",
+                      model_averaging = model_averaging,
+                      num_bootstraps = num_bootstraps)
 
   if (summary == TRUE) {
     results_df <- results[[2]]
