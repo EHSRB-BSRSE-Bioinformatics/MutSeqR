ef2fa0211cf06095aacb3607481b64f0934932e8
Annette Dodge <annettedodge16@gmail.com>
2025-03-05 16:16:21 -0500

    write_reference_fasta examples and doc

--- R/write_reference_fasta.R
+++ R/write_reference_fasta.R
@@ -1,22 +1,42 @@
-#' Write FASTA file of reference sequences
+#' Write FASTA file of reference sequences.
 #'
-#' In some cases, you might want to generate an arbitrary multi-sequence FASTA
-#' file from GRanges including the reference sequences. This function allows you
-#' to do this.
-#' @param ref_ranges A GRanges object including the sequences of the reference
-#' regions included for the data.
-#' @param fasta_out The path for the FASTA file output.
-#' @returns Writes a FASTA reference file to be used in downstream applications.
+#' @details Generate an arbitrary multi-sequence FASTA file from GRanges
+#' including the reference sequences.
+#' @param regions_gr A GRanges object including the sequences of the reference
+#' regions included for the data. This can be generated from the `get_seq`
+#' function.
+#' @param output_path The directory where the FASTA file should be written.
+#' Default is NULL, which will write the file to the current working directory.
+#' @returns Writes a FASTA reference file "reference_output.fasta".
+#' If multiple ranges are included in the GRanges object, the sequences will be
+#' written to a single FASTA file. Sequences names will be the seqnames
+#' (contig) of the range.
+#' @examples
+#' \dontrun{
+#' # Write FASTA files for the 20 genomic target sequences
+#' # of TwinStrand's Mouse Mutagenesis Panel.
+#' rg <- get_seq("TSpanel_mouse")
+#' write_reference_fasta(rg, output_path = NULL)
+#' }
 #' @importFrom Biostrings writeXStringSet
 #' @export
-write_reference_fasta <- function(ref_ranges,
-                                  fasta_out = "./reference_output.fasta") {
-  ref <- ref_ranges$sequence
+write_reference_fasta <- function(regions_gr,
+                                  output_path = NULL) {
+  if (is.null(output_path)) {
+    output_dir <- file.path(here::here(), "reference_output.fasta")
+  } else {
+    output_dir <- file.path(output_path, "reference_output.fasta")
+  }
+  if (!dir.exists(output_dir)) {
+    dir.create(output_dir)
+    output_dir <- file.path(output_dir, "reference_output.fasta")
+  }
+  ref <- regions_gr$sequence
   # Convert to DNAStringSet
   ref <- Biostrings::DNAStringSet(ref)
-  names(ref) <- make.unique(as.vector(seqnames(ref_ranges)))
+  names(ref) <- make.unique(as.vector(seqnames(regions_gr)))
   Biostrings::writeXStringSet(ref,
-                  fasta_out,
-                  append=FALSE,
-                  format="fasta")
+                              output_dir,
+                              append = FALSE,
+                              format = "fasta")
 }
