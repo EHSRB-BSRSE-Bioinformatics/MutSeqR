c6ef0e205c4c8c63dc5ac10cee03a6d94c2fab77
Matthew Meier <matthew.james.meier@gmail.com>
2025-06-17 07:04:09 -0400

    Remove PROAST reference in global variables

--- R/globals.R
+++ R/globals.R
@@ -1,17 +1,7 @@
-# This file tells R CMD check about "global" variables that are used
-# in the bundled PROAST code. This is necessary to avoid NOTES during
-# the check process, as these PROAST variables are not used by MutSeqR
-
+# This file tells R CMD check about variables used in the context
+# of non-standard evaluation
 utils::globalVariables(
   names = c(
-    # # PROAST internal functions
-    # "f.adjust.saved", "f.cat", "f.ced.cat", "f.CI.sel.ord", "f.delete.gw",
-    # "f.dtype6.mn", "f.expect.cat", "f.firstcheck", "f.nested.con",
-    # "f.plot.cxt", "f.plot.sep", "f.press.key.to.continue", "f.quick.cat",
-    # "f.select.m46.con", "f.show.cat", "f.start.cat", "f.use.comma.for.decimal",
-
-    # # PROAST internal state variables
-    # ".gw.size", ".Pr.last", ".ypos"
     "priority_order", "variation_type", "total_depth", ".N", ".", "contig", 
     "proportion_min", "proportion_max", "CED", "Selected.Model", "CEDL", "CEDU", 
     "weights", "dum", "vaf", "filter_reason", "in_regions", "x.circle_coords", 
