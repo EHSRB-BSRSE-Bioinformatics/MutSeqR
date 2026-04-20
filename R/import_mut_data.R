5dbe4c07721d338a98b5e25c757af9110322f7f7
mattjmeier <matthew.james.meier@gmail.com>
2026-04-20 07:22:19 -0400

    fix: order of checking was not correct

--- R/import_mut_data.R
+++ R/import_mut_data.R
@@ -361,11 +361,6 @@ import_mut_data <- function(
 
   if (!context_exists) {
     validate_BS_genome(BS_genome)
-
-    mut_ranges <- populate_sequence_context(
-      mutation_granges = mut_ranges,
-      BS_genome = BS_genome
-    )
   }
 
   # Turn mutation data into GRanges
